5427353b3ad2fd22609a9551f8a68ef6ab997eeb
m-haketa <git@excelspeedup.com>
2019-06-22 10:33:29 +0900

    最低限の設定完了

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  el: '#app',
+  data: {
+    maxX: 10,
+    maxY: 5,
+    NumberOfMine: 5,
+    cells: [] as any[]
+  },
+  computed: {
+    initialized: function() {
+      return this.cells.length > 0
+    }
+  },
+  methods: {
+    init: function() {
+      let mineArr = []
+      this.cells = []
+
+      for (var i = 0; i < this.maxX * this.maxX; i++) {
+        this.cells = Array.apply(null, {
+          length: this.maxX * this.maxY
+        } as any).map(function(_value: any, index: number) {
+          return index
+        })
+      }
+    }
+  }
+})
